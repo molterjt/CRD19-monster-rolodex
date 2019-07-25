0346a974bec829a127dca5cc0c687ba0eea07848
jeffreyT <molterjt@miamioh.edu>
2019-07-25 10:17:57 -0400

    Updates

---
+++ precache-manifest.0be9d15bca7c6dcf4f321a6c738b25bb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f83564e6c23e656b771384637357ef1",
+    "url": "/CRD19-monster-rolodex/index.html"
+  },
+  {
+    "revision": "bd4ddd7006beaaeacb27",
+    "url": "/CRD19-monster-rolodex/static/css/main.f10dd375.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/CRD19-monster-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "bd4ddd7006beaaeacb27",
+    "url": "/CRD19-monster-rolodex/static/js/main.88acd158.chunk.js"
+  },
+  {
+    "revision": "0959fd661caca8a4a2d0",
+    "url": "/CRD19-monster-rolodex/static/js/runtime~main.87303b8b.js"
+  }
+]);
